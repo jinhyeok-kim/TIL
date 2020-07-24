69921f42b820f65552a6cebb783f60eef86fe2df
jinhyeok <persona5173@gmail.com>
2020-07-25 01:27:58 +0900

    multi information

--- python_tutorial/crawling.py
+++ python_tutorial/crawling.py
@@ -1,12 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://finance.naver.com/item/main.nhn?code=005380"
-result = requests.get(url)
-bs_obj = BeautifulSoup(result.content, "html.parser")
+finance_list = []
 
-no_today = bs_obj.find("p", {"class": "no_today"})
-blind = no_today.find("span", {"class": "blind"})
-now_price = blind.text
- 
-print(now_price)
+finance_list.append("https://finance.naver.com/item/main.nhn?code=272210")
+# finance_list.append("https://finance.naver.com/item/main.nhn?code=005380")
+
+for url in finance_list:
+    result = requests.get(url)
+    bs_obj = BeautifulSoup(result.content, "html.parser")
+
+    no_today = bs_obj.find("p", {"class": "no_today"})
+    blind = no_today.find("span", {"class": "blind"})
+    now_price = blind.text
+
+    print(now_price)
